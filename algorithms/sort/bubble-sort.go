72425651247ef1cd384760405379e72c485bda90
peiyu <peiyu@roobo.com>
2018-03-21 01:24:38 +0800

    fixbug: SimpoleBubbleSort:the sort times should be len(array)-1

--- algorithms/sort/bubble-sort.go
+++ algorithms/sort/bubble-sort.go
@@ -5,7 +5,7 @@ package sort
 
 func SimpleBubbleSort(array []int) {
 	checkArray(array)
-	for i := 0; i < len(array); i++ {
+	for i := 0; i < len(array)-1; i++ {
 		for j := 1; j < len(array) - i; j++ {
 			if array[j - 1] > array[j] {
 				swap(array, j - 1, j)
