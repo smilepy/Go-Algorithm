457602351db3652e701e69d71d0bea3075c69b6f
peiyu <peiyu@roobo.com>
2018-03-28 08:41:29 +0800

    optimize insert-sort

--- algorithms/sort/insert-sort.go
+++ algorithms/sort/insert-sort.go
@@ -8,12 +8,13 @@ func InsertSort(array []int) {
 	// The array is divided into two parts: sorted-part and unsorted-part. 
 	// At the beginning, sorted-part contains the first element, and unsorted-part contains the rest.
 	for i := 1; i < len(array); i++ {
-		// For every element in unsorted-part,
-		for j := i; j > 0; j-- {
-			// choose the right place in sorted-part to insert.
-			if array[j - 1] > array[j] {
-				swap(array, j - 1, j)
-			}
+		// For every element in unsorted-part
+		j:=i-1
+		current:=array[i]
+		for ; j >= 0&& array[j]>current; j-- {
+				array[j+1]=array[j]
 		}
+		// choose the right place in sorted-part to insert.
+		array[j+1]=current
 	}
 }
